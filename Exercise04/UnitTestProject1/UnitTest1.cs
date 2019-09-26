b6473c9404e81bf290ca368768ed5e1ddcd655ef
ThanhTanPM2000 <thanhtanpm2000@gmail.com>
2019-09-26 15:17:03 +0700

    TestUnit

---
+++ Exercise04/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise04;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+        }
+
+        
+
+        [TestMethod]
+        public void IsEven()
+        {
+            bool result1 = Program.IsEven(4);
+            Assert.AreEqual(true, result1);
+
+            bool result2 = Program.IsEven(3);
+            Assert.AreEqual(false, result2);
+        }
+        [TestMethod]
+        public void IsPrime()
+        {
+            bool result1 = Program.IsPrime(5);
+            Assert.AreEqual(true, result1);
+
+            bool result2 = Program.IsPrime(4);
+            Assert.AreEqual(false, result2);
+        }
+
+        [TestMethod]
+        public void Square()
+        {
+            int result1 = Program.Square(4);
+            Assert.AreEqual(16, result1);
+
+            int result2 = Program.Square(3);
+            Assert.AreEqual(9, result2);
+        }
+
+        [TestMethod]
+        public void Cube()
+        {
+            int result1 = Program.Cube(4);
+            Assert.AreEqual(64, result1);
+
+            int result2 = Program.Cube(3);
+            Assert.AreEqual(27, result2);
+        }
+    }
+}

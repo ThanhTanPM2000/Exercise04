1dc3cebed2de037410b916f6b41d5d06eb02faf3
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2019-10-01 15:30:35 +0700

    Exercise04b

---
+++ blabla/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blabla;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestContain5()
+        {
+            int[] Arr1 = new int[] { 1, 2, 3, 4 };
+            bool result5 = Program.IsContain5(Arr1);
+            Assert.AreEqual(false, result5);
+
+            int[] Arr2 = new int[] { 1, 2, 3, 4,5 };
+            bool result0 = Program.IsContain5(Arr2);
+            Assert.AreEqual(true, result0);
+        }
+
+        [TestMethod]
+        public void TestContain5or6()
+        {
+            int[] Arr1 = new int[] { 1, 2, 3, 4 };
+            bool result5 = Program.IsContain5or6(Arr1);
+            Assert.AreEqual(false, result5);
+
+            int[] Arr2 = new int[] { 1, 2, 3, 4, 5 };
+            bool result5or6 = Program.IsContain5or6(Arr2);
+            Assert.AreEqual(true, result5or6);
+        }
+
+        [TestMethod]
+        public void TestContain5And6()
+        {
+            int[] Arr1 = new int[] { 1, 2, 3, 4 };
+            bool result5 = Program.IsContain5And6(Arr1);
+            Assert.AreEqual(false, result5);
+
+            int[] Arr2 = new int[] { 1, 2, 3, 4, 5 ,6};
+            bool result5and6 = Program.IsContain5And6(Arr2);
+            Assert.AreEqual(true, result5and6);
+        }
+    }
+}
---
+++ blabla/blabla/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blabla
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[10];
+            InputInt(Arr);
+            Console.ReadLine();
+        }
+        public static void InputInt(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                string str = Console.ReadLine();
+                Arr[i] = int.Parse(str);
+            }
+        }
+
+        public static bool IsContain5(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i] == 5) return true;
+            }
+            return false;
+        }
+
+        public static bool IsContain5or6(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i] == 5 || Arr[i] == 6) return true;
+            }
+            return false;
+        }
+
+        public static bool IsContain5And6(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i] == 5 && Arr[i] == 6) return true;
+            }
+            return false;
+        }
+
+    }
+}

5084c626e172046b1c0c10b22258e4aed19b84f2
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2019-10-01 14:43:43 +0700

    Exercise4

--- Exercise04/UnitTestProject2/Properties/AssemblyInfo.cs
+++ Exercise04/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("UnitTestProject2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("UnitTestProject2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9ed7f4db-df49-4209-90df-98b1bacf31e5")]
+[assembly: Guid("323e2228-c1e2-4839-a1e4-a01aeb038ae9")]
 
 // Version information for an assembly consists of the following four values:
 //
